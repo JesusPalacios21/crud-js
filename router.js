198cbecee2518aee7e9de388ef566cd2bc35456f
DayerAlvarez <dayeralvarez2004@gmail.com>
2025-04-21 15:31:02 -0500

    Proyecto Finalizado

--- router.js
+++ router.js
@@ -24,8 +24,31 @@ router.get('/create', (req, res) => {
   res.render('create');
 });
 
+router.get('/delete/:id', (req, res) => {
+  const idvehiculo = req.params.id;
+  conexion.query("DELETE FROM vehiculos WHERE id= ?", [idvehiculo], (error, results) => {
+    if (error){
+      throw(error);
+    }else{  
+      res.redirect('/');
+    }
+  });
+});
+
+//Para editar debemos identificar el registro
+router.get('/edit/:id', (req,res) => {
+  conexion.query("SELECT * FROM vehiculos WHERE id= ?", [req.params.id], (error, results) => {
+    if(error){
+      throw(error);
+    }else{
+      res.render('edit', {vehiculo: results[0]});
+    }
+  });
+});
+
 //Acceder a toda la logica
 const crud = require('./controllers/crud');
-router.post('/save', crud.save)
+router.post('/save', crud.save);
+router.post('/update', crud.update);
 
 module.exports = router;
